7af3c2d482d91946b8d9bba2bc0f53ed1727f757
Daniel Svensson <dvensson@gmail.com>
2022-05-16 00:30:04 +0200

    Drop iOS support.

--- src/pages/match.js
+++ src/pages/match.js
@@ -3,15 +3,21 @@ import Layout from "../components/layout"
 import Seo from "../components/seo"
 import FteComponent from "../components/fte"
 import DiscordButton from "../components/discord"
-import browser from "browser-detect"
 
+function iOS() {
+  return (
+    ["iPad Simulator", "iPhone Simulator", "iPod Simulator", "iPad", "iPhone", "iPod"].includes(navigator.platform) ||
+    // iPad on iOS 13 detection
+    (navigator.userAgent.includes("Mac") && "ontouchend" in document)
+  )
+}
 
 const MatchPage = ({ pageContext: { demo, map } }) => {
   return (
     <Layout>
       <Seo title="demo" />
-      <FteComponent demo={demo} map={map}/>
-      <DiscordButton/>
+      {iOS() ? "iOS browser performance is really bad, please fix, or use Desktop or Android" : <FteComponent demo={demo} map={map} />}
+      <DiscordButton />
     </Layout>
   )
 }
