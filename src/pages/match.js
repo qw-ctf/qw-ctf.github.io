83c9263b021ec373b7035d73e543099a3bb5f768
Daniel Svensson <dvensson@gmail.com>
2022-05-16 01:02:22 +0200

    Fix missing navigator object at compile time.

--- src/pages/match.js
+++ src/pages/match.js
@@ -3,12 +3,15 @@ import Layout from "../components/layout"
 import Seo from "../components/seo"
 import FteComponent from "../components/fte"
 import DiscordButton from "../components/discord"
+import { window, document } from 'browser-monads';
+
 
 function iOS() {
+
   return (
-    ["iPad Simulator", "iPhone Simulator", "iPod Simulator", "iPad", "iPhone", "iPod"].includes(navigator.platform) ||
+    ["iPad Simulator", "iPhone Simulator", "iPod Simulator", "iPad", "iPhone", "iPod"].includes(window.navigator.platform) ||
     // iPad on iOS 13 detection
-    (navigator.userAgent.includes("Mac") && "ontouchend" in document)
+    (window.navigator.userAgent.includes("Mac") && "ontouchend" in document)
   )
 }
 
