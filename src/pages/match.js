65de3fd537634b81a29aed98137a6774366e58be
Daniel Svensson <dvensson@gmail.com>
2022-05-29 00:22:17 +0200

    Add share/download links.
    Share still not doing anything.

--- src/pages/match.js
+++ src/pages/match.js
@@ -2,11 +2,12 @@ import * as React from "react"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import FteComponent from "../components/fte"
-import { window, document } from 'browser-monads';
-
+import { window, document } from "browser-monads"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faShare, faDownload } from "@fortawesome/free-solid-svg-icons"
+import * as matchStyle from "./match.module.scss"
 
 function iOS() {
-
   return (
     ["iPad Simulator", "iPhone Simulator", "iPod Simulator", "iPad", "iPhone", "iPod"].includes(window.navigator.platform) ||
     // iPad on iOS 13 detection
@@ -15,11 +16,32 @@ function iOS() {
 }
 
 const MatchPage = ({ pageContext: { demo, map, directory, duration } }) => {
+  const baseUrl = "https://media.githubusercontent.com/media/qw-ctf/matches/main/"
+  const demoUrl = `${baseUrl}${directory}/${encodeURIComponent(demo)}.gz`
+
   return (
     <Layout>
       <Seo title="demo" />
-      {iOS() ? "iOS browser performance is really bad, please fix, or use Desktop or Android" : <FteComponent demo={demo} map={map.name} directory={directory} duration={duration}/>}
-      <h1>{map.name.toUpperCase()}: {map.metadata.name} by {map.metadata.author || "Unknown"}</h1>
+      {iOS() ? (
+        "iOS browser performance is really bad, please fix, or use Desktop or Android"
+      ) : (
+        <FteComponent demo={demo} map={map.name} directory={directory} duration={duration} />
+      )}
+      <div className={matchStyle.matchInfo}>
+        <div className={matchStyle.mapName}>
+          {map.name.toUpperCase()}: {map.metadata.name} by {map.metadata.author || "Unknown"}
+        </div>
+        <div className={matchStyle.share}>
+          <FontAwesomeIcon icon={faShare} />
+          Share
+        </div>
+        <div className={matchStyle.download}>
+          <a href={demoUrl}>
+          <FontAwesomeIcon icon={faDownload} />
+          Download
+          </a>
+        </div>
+      </div>
     </Layout>
   )
 }
